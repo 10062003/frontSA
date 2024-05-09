7e8226984731cd52143feb84a4e51fe4a9472ca8
Kevin Holguín <kee123371@gmail.com>
2024-05-08 19:35:18 -0500

    Integración de CAPTCHA

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,15 +1,34 @@
+import { useReducer } from "react";
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
 import Label from "../components/ui/Label";
 import LogoIcon from "./icons/LogoIcon";
-import { MailIcon } from "./icons/MailIcon";
+import {MailIcon} from "./icons/MailIcon";
 import PassIcon from "./icons/PassIcon";
+import ReCAPTCHA from "react-google-recaptcha";
+import React, {useRef} from 'react';
 
 const Login = () => {
+
+  const captcha=useRef(null);
+
+  const onChange = () => {
+    if(captcha.current.getValue()){
+    console.log("Algo cambió desde la última vez.");
+    }
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log('Se envio el formulario.')
+  }
+
   return (
     <>
+     
       <div className="flex min-h-full h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8dark:bg-neutral-900">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          
           <LogoIcon className="mx-auto h-12 w-auto dark:text-green-600" />
           <h2 className="mt-5 text-center text-4xl font-bold leading-9 tracking-tight text-green-600">
             Super Administrador
@@ -17,7 +36,7 @@ const Login = () => {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" action="#" method="POST" onSubmit={submit}>
             <div>
               <Label htmlFor="email">Correo Institucional</Label>
               <div className="mt-2">
@@ -63,6 +82,16 @@ const Login = () => {
                 </a>
               </div>
             </div>
+            
+            <div className="flex justify-center items-center">
+                <div className="recaptcha">
+                    <ReCAPTCHA  
+                        ref={captcha}    
+                        sitekey="6LeNTm8pAAAAAJ8LiwLy1QSgHNWPKeBR4XZoKsqO"
+                        onChange={onChange}
+                      />
+                  </div>
+             </div>  
 
             <div className="flex justify-center">
               <Button type="submit">Iniciar sesión</Button>
@@ -70,6 +99,7 @@ const Login = () => {
           </form>
         </div>
       </div>
+    
     </>
   );
 };
