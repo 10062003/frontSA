3afe2ea7a0251f9f4612156ac0e0dc56e2d4dcfc
Daniel Paternina Ortega <paternina1006@gmail.com>
2024-05-09 00:43:18 -0500

    Se creo la SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import { LayoutDashboard, SidebarIcon } from "lucide-react";
+import {
+  BarChart3,
+  BellRing,
+  Clock,
+  Component,
+  Drama,
+  GraduationCap,
+  Home,
+  LayoutDashboard,
+  NotepadText,
+  ScrollText,
+  Settings,
+  SidebarIcon,
+  Tags,
+} from "lucide-react";
 import Login from "./Login/Login";
-import Sidebar, { SidebarItem } from "./Sidebar/SideBar";
+import Sidebar, { SidebarItem } from "./Sidebar/Sidebar";
 
 const App = () => {
   return (
-    <div className="h-screen dark:bg-neutral-800">
-      <Login />
-      {/* <Sidebar /> */}
-      {/* <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" /> */}
+    <div className=" flex dark:bg-neutral-800">
+      <Sidebar>
+        <SidebarItem icon={<Home size={20} />} text="Inicio" />
+        <SidebarItem icon={<BarChart3 size={20} />} text="Upas" />
+        <SidebarItem icon={<BellRing size={20} />} text="Actividades" />
+        <SidebarItem icon={<GraduationCap size={20} />} text="Profesiones" />
+        <SidebarItem icon={<Component size={20} />} text="Modulos" />
+        <SidebarItem icon={<NotepadText size={20} />} text="Estados" />
+        <SidebarItem icon={<Drama size={20} />} text="Roles" />
+        <SidebarItem icon={<Tags size={20} />} text="Ticket" />
+        <SidebarItem icon={<Clock size={20} />} text="Historial" />
+        <SidebarItem icon={<Settings size={20} />} text="Configuración" alert />
+      </Sidebar>
+      <div className="flex-1">
+        <Login />
+      </div>
     </div>
   );
 };
