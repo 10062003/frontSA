c5daaa9a4cfe1e01fa12b45055c6d8264334c2b6
Daniel Paternina Ortega <paternina1006@gmail.com>
2024-06-09 15:36:11 -0500

    inputs

---
+++ src/components/ui/InputBasic.jsx
@@ -0,0 +1,10 @@
+const InputBasic = (props, className) => {
+  return (
+    <input
+      className={`input-placeholder w-full text-[13px] bg-white border-2 border-gray-300 rounded-md h-11 leading-[45px] px-[10px] pr-[40px] pl-[40px] transition-all duration-200 ease-in-out focus:border-blue-400 focus:outline-none focus:shadow-[3px_0px_30px_rgba(163,163,163,0.4)] dark:bg-neutral-800 dark:text-white dark:border-neutral-100 ${className}`}
+      {...props}
+    />
+  );
+};
+
+export default InputBasic;
